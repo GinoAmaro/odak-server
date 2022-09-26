e78ef00125dab22b6f6ec6564e278d4995392230
Gino Amaro <emzero1@gmail.com>
2022-09-26 08:50:39 -0300

    Primer commit

---
+++ conexion.php
@@ -0,0 +1,17 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+function Conectar()
+{
+    $servidor = "localhost";
+    $usuario = "root";
+    $contrasenia = "";
+    $nombreBaseDatos = "odak";
+    $conexionBD = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);
+    return $conexionBD;
+}
