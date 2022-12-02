740314be52912bd56b015c321ecb23b49decd620
Gino Amaro <emzero1@gmail.com>
2022-12-02 20:40:19 -0300

    Ultimo

--- login/correo.php
+++ login/correo.php
@@ -20,7 +20,7 @@ function enviarCorreo($correo,$token,$usuario)
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'emzero1@gmail.com';                     //SMTP username
-        $mail->Password   = 'ltkittlwkydwfmvr';                               //SMTP password
+        $mail->Password   = '';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
